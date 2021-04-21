0e1c01493508ed92c9aebd575b85cd215a3bde8b
fdznoriega <andresfdz7@gmail.com>
2021-04-21 00:59:46 -0400

    navbar draft

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,42 @@
 
 
 function Navbar(props) {
+
+    function handleNavChange(page){
+        if (props.onNavChange){
+            console.log("Redirecting to: " + page);
+            props.onNavChange(page);
+        }
+    }
+
     return (
         <nav>
-            <div>Water</div>
-            <div>Food</div>
-            <div>New Post</div>
-            <div>Housing</div>
-            <div>Wild Card</div>
+
+            <div>
+                <button onClick={e => handleNavChange("Water")}>
+                    Water
+                </button>
+            </div>
+            <div>
+                <button onClick={e => handleNavChange("Food")}>
+                    Food
+                </button>
+            </div>
+            <div>
+                <button onClick={e => handleNavChange("NewPost")}>
+                    New Post
+                </button>
+            </div>
+            <div>
+                <button onClick={e => handleNavChange("Housing")}>
+                    Housing
+                </button>
+            </div>
+            <div>
+                <button onClick={e => handleNavChange("WildCard")}>
+                    Wild Card
+                </button>
+            </div>
         </nav>
     );
 }

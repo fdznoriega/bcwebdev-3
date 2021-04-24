498e34d91499d621d604110b439f8611a6427f77
Ronan Manvelian <ronanmanvelian@gmail.com>
2021-04-24 12:44:17 -0700

    Updated Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,26 +15,31 @@ function Navbar(props) {
                 <button onClick={e => handleNavChange("Water")}>
                     <i className="fas fa-tint"></i>
                 </button>
+                <p className={css.navText}>Water</p>
             </div>
             <div className={css.navItem}>
                 <button onClick={e => handleNavChange("Food")}>
                     <i className="fas fa-utensils"></i>
                 </button>
+                <p className={css.navText}>Food</p>
             </div>
             <div className={css.navItem}>
                 <button onClick={e => handleNavChange("NewPost")}>
                     <i className="fas fa-plus-square"></i>
                 </button>
+                <p className={css.navText}>New Post</p>
             </div>
             <div className={css.navItem}>
                 <button onClick={e => handleNavChange("Housing")}>
                     <i className="fas fa-home"></i>
                 </button>
+                <p className={css.navText}>Housing</p>
             </div>
             <div className={css.navItem}>
                 <button onClick={e => handleNavChange("WildCard")}>
                     <i className="fas fa-vr-cardboard"></i>
                 </button>
+                <p className={css.navText}>Wild Card</p>
             </div>
         </nav>
     );

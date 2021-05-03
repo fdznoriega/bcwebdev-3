bdfaa0ecb053a5c9761abf0ca1b08830243d8a88
Ronan Manvelian <ronanmanvelian@gmail.com>
2021-05-02 17:10:33 -0700

    Additional style changes

--- src/components/PostPreview.js
+++ src/components/PostPreview.js
@@ -73,7 +73,7 @@ function PostPreview(props) {
                 return <span className="fas fa-warehouse cat"></span>;
             case "Water":
                 return <span className="fas fa-tint cat"></span>;
-            case "Food": 
+            case "Food":
                 return <span className="fas fa-utensils cat"></span>;
         }
     }
@@ -116,12 +116,12 @@ function PostPreview(props) {
                     {/* likes and comments */}
                     <section>
                         <div className={css.like_info}>
-                            <span>{likeCount}</span>
+                            <span className={css.count}>{likeCount}</span>
                             {renderLikeIcon()}
                         </div>
 
                         <div className={css.comment_info}>
-                            <span>{commentCount}</span>
+                            <span className={css.count}>{commentCount}</span>
                             <Link to={"/post/" + post.id}>
                                 <i className="fas fa-comment"></i>
                             </Link>
